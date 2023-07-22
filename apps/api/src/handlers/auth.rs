72de611f5bb4beef0a9d840e1e14e1e6e67dbe85
Nurliman Diara <nurlimandiara@gmail.com>
2023-07-22 14:14:25 +0700

    simplify the code for auth

--- apps/api/src/handlers/auth.rs
+++ apps/api/src/handlers/auth.rs
@@ -1,26 +1,27 @@
 use crate::{
-    errors::{self, AppError},
-    models, services,
+    errors::AppError,
+    models::{CredentialUser, RefreshToken, SignIn, User},
+    services,
     validation::JsonOrForm,
 };
 use axum::Json;
 
-pub async fn list_users() -> Result<Json<models::User>, AppError> {
+pub async fn list_users() -> Result<Json<User>, AppError> {
     let users = services::users::get_user_by_id("asd").await?;
     Ok(Json(users))
 }
 
 pub async fn sign_in(
-    JsonOrForm(body): JsonOrForm<models::SignIn>,
-) -> Result<Json<models::CredentialUser>, AppError> {
+    JsonOrForm(body): JsonOrForm<SignIn>,
+) -> Result<Json<CredentialUser>, AppError> {
     let user = services::auth::sign_in(&body.username, &body.password).await?;
     Ok(Json(user))
 }
 
 pub async fn refresh_token(
-    JsonOrForm(body): JsonOrForm<models::RefreshToken>,
-) -> Result<Json<models::CredentialUser>, AppError> {
-    let refresh_token = body.refresh_token.ok_or(errors::AppError::InternalError)?;
+    JsonOrForm(body): JsonOrForm<RefreshToken>,
+) -> Result<Json<CredentialUser>, AppError> {
+    let refresh_token = body.refresh_token.ok_or(AppError::InternalError)?;
     let user = services::auth::refresh_token(&refresh_token).await?;
     Ok(Json(user))
 }
