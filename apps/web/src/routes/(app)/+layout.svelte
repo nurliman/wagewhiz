98d3a3e92da357fc2ba814bf2aba58e40e3491f2
nurliman <nurlimandiara@gmail.com>
2023-08-10 23:44:27 +0700

    init login page

---
+++ apps/web/src/routes/(app)/+layout.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import Navbar from "$lib/components/Navbar.svelte";
+  import Sidebar from "$lib/components/Sidebar.svelte";
+  import Footer from "$lib/components/Footer.svelte";
+  import type { LayoutData } from "./$types.ts";
+
+  export let data: LayoutData;
+</script>
+
+<Navbar />
+
+<div class="flex h-full relative pt-16">
+  <Sidebar />
+  <main class="flex flex-col h-full w-full lg:ml-64">
+    <slot />
+    <div class="flex-1" />
+    <Footer currentYear={data.currentDate?.getFullYear?.()} />
+  </main>
+</div>
--- apps/web/src/routes/+layout.svelte
+++ apps/web/src/routes/+layout.svelte
@@ -3,25 +3,10 @@
   import "@skeletonlabs/skeleton/styles/skeleton.css";
   import "../app.postcss";
   import "../theme.postcss";
-  import Navbar from "$lib/components/Navbar.svelte";
-  import Sidebar from "$lib/components/Sidebar.svelte";
-  import Footer from "$lib/components/Footer.svelte";
   import { computePosition, autoUpdate, offset, shift, flip, arrow } from "@floating-ui/dom";
   import { storePopup } from "@skeletonlabs/skeleton";
-  import type { LayoutData } from "./$types.ts";
 
   storePopup.set({ computePosition, autoUpdate, offset, shift, flip, arrow });
-
-  export let data: LayoutData;
 </script>
 
-<Navbar />
-
-<div class="flex h-full relative pt-16">
-  <Sidebar />
-  <main class="flex flex-col h-full w-full lg:ml-64">
-    <slot />
-    <div class="flex-1" />
-    <Footer currentYear={data.currentDate?.getFullYear?.()} />
-  </main>
-</div>
+<slot />
