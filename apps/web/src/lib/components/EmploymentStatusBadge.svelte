82f1fbf503a5e005c42f939f18cd2bd1b8ed9db5
nurliman <nurlimandiara@gmail.com>
2023-08-25 23:41:50 +0700

    refactor `EmploymentStatusBadge`

--- apps/web/src/lib/components/EmploymentStatusBadge.svelte
+++ apps/web/src/lib/components/EmploymentStatusBadge.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import { clsx } from "clsx";
   import type { EmployeeStatus } from "$lib/types.ts";
 
   export let status: EmployeeStatus;
@@ -6,27 +7,32 @@
 
 <div
   {...$$restProps}
-  class={`badge variant-ghost ` + $$restProps.class}
-  class:permanent={status === "Permanent"}
-  class:contract={status === "Contract"}
-  class:probation={status === "Probation"}
+  class={clsx(
+    "badge variant-ghost",
+    {
+      "status-badge-permanent": status === "Permanent",
+      "status-badge-contract": status === "Contract",
+      "status-badge-probation": status === "Probation",
+    },
+    $$restProps.class,
+  )}
 >
   {status}
 </div>
 
 <style lang="postcss">
-  .badge {
-    &.permanent {
+  .status-badge {
+    &-permanent {
       @apply ring-[1px] ring-green-500/80 dark:ring-green-500/80 ring-inset;
       @apply bg-green-500/20 dark:bg-green-500/20;
     }
 
-    &.contract {
+    &-contract {
       @apply ring-[1px] ring-yellow-500/80 dark:ring-yellow-500/80 ring-inset;
       @apply bg-yellow-500/20 dark:bg-yellow-500/20;
     }
 
-    &.probation {
+    &-probation {
       @apply ring-[1px] ring-purple-500/80 dark:ring-purple-500/80 ring-inset;
       @apply bg-purple-500/20 dark:bg-purple-500/20;
     }
