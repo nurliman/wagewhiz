9183125997cc9470178597cedf471a399b97aafe
nurliman <nurlimandiara@gmail.com>
2023-08-25 23:34:14 +0700

    create Spinner component

---
+++ apps/web/src/lib/components/Spinner.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+  import { clsx } from "clsx";
+  type Variant = "primary" | "secondary" | "tertiary";
+  export let containerClass: string = "";
+  export let variant: Variant = "secondary";
+</script>
+
+<div class={clsx("flex justify-center items-center h-12 w-12 animate-spin", containerClass)}>
+  <div class={clsx(`the-spinner the-spinner-${variant}`, $$restProps.class)}></div>
+</div>
+
+<style lang="postcss">
+  .the-spinner {
+    display: block;
+    margin: auto;
+    height: 100%;
+    width: 100%;
+    border-width: 6px;
+    border-radius: 50%;
+    animation: rotation 0.6s infinite linear;
+  }
+
+  .the-spinner-primary {
+    @apply border-primary-900 border-t-primary-500;
+  }
+
+  .the-spinner-secondary {
+    @apply border-secondary-900 border-t-secondary-500;
+  }
+
+  .the-spinner-tertiary {
+    @apply border-tertiary-900 border-t-tertiary-500;
+  }
+
+  @keyframes rotation {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(359deg);
+    }
+  }
+</style>
---
+++ apps/web/src/lib/components/SpinnerPage.svelte
@@ -0,0 +1,7 @@
+<script>
+  import Spinner from "./Spinner.svelte";
+</script>
+
+<div class="flex h-full justify-center items-center flex-1">
+  <Spinner containerClass="w-16 h-16" />
+</div>
