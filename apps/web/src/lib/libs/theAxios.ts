0dc3f4bf5bebc29fcb5206079b958026b6368f31
nurliman <nurlimandiara@gmail.com>
2023-08-27 20:41:24 +0700

    implementation auth for api client

--- apps/web/src/lib/libs/theAxios.ts
+++ apps/web/src/lib/libs/theAxios.ts
@@ -1,15 +1,51 @@
+import endsWith from "lodash/endsWith";
 import axios from "axios";
-import _axiosRetry from "axios-retry";
+import axiosRetry from "axios-retry";
+import { goto } from "$app/navigation";
 
-const axiosRetry = _axiosRetry as unknown as typeof _axiosRetry.default;
+const BACKEND_BASE_URL = "http://localhost:3001";
 
 const theAxios = axios.create({
-  // TODO: use env var
-  baseURL: "http://localhost:3001",
+  baseURL: BACKEND_BASE_URL,
+  timeout: 5000,
+  paramsSerializer: {
+    indexes: null,
+  },
+  headers: {
+    "Content-Type": "application/json",
+  },
+  withCredentials: true,
 });
 
-axiosRetry(theAxios, { retries: 3 });
+axiosRetry(theAxios, {
+  retries: 3,
+  shouldResetTimeout: true,
+  retryDelay: axiosRetry.exponentialDelay,
+  retryCondition: async (error) => {
+    if (error.config?.url?.includes("sign-in") || error.config?.url?.includes("refresh-token")) {
+      return axiosRetry.isNetworkError(error) || error.code === "ECONNABORTED";
+    }
+    if (error.response?.status !== 401) {
+      return axiosRetry.isNetworkOrIdempotentRequestError(error) || error.code === "ECONNABORTED";
+    }
+    const res = await theAxios
+      .post(`${BACKEND_BASE_URL}/v0/auth/refresh-token`, {})
+      .catch((refreshTokenError) => {
+        if ([400, 401].includes(refreshTokenError.response?.status)) {
+          let loginUrl = "/login";
 
-// TODO: handle refresh token
+          if (window.location.href && !endsWith(window.location.href, "/login")) {
+            loginUrl += `?redirect=${window.location.href}`;
+          }
+
+          goto(loginUrl);
+        }
+      });
+    if (!res?.data?.credential?.access_token) return false;
+
+    // TODO: update auth state here
+    return true;
+  },
+});
 
 export { theAxios };
