3a033d385ceba60e27b32c2a06c7eae18ea6a1f2
nurliman <nurlimandiara@gmail.com>
2023-08-28 23:33:53 +0700

    style login page

--- apps/web/src/routes/(auth)/login/+page.svelte
+++ apps/web/src/routes/(auth)/login/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import axios from "axios";
+  import { isAxiosError } from "axios";
   import { goto } from "$app/navigation";
   import { nanoid } from "$lib/utils/nanoid.ts";
   import { createMutation } from "@tanstack/svelte-query";
@@ -25,7 +25,7 @@
       await goto(redirectUrl);
     },
     onError: (err) => {
-      if (axios.isAxiosError(err)) {
+      if (isAxiosError(err)) {
         if (err.response?.data?.message) {
           toastStore.trigger({
             message: err.response.data.message,
@@ -54,84 +54,56 @@
   });
 </script>
 
-<section class="bg-gray-50 dark:bg-gray-900">
+<section>
   <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-    <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
-      <img
-        class="w-8 h-8 mr-2"
-        src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg"
-        alt="logo"
-      />
-      Flowbite
+    <a href="/" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+      {"[ WageWhiz ]"}
     </a>
-    <div
-      class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700"
-    >
+    <div class="card w-full sm:max-w-md">
       <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-        <h1
-          class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white"
-        >
-          Sign in to your account
-        </h1>
+        <h1 class="h3 mb-4 font-bold">Sign in to your account</h1>
         <form class="space-y-4 md:space-y-6" method="POST" use:enhance>
-          <div>
-            <label
-              for={fieldUsernameId}
-              class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-            >
-              Username
-            </label>
+          <div class="space-y-2">
+            <label for={fieldUsernameId} class="label"> Username </label>
             <input
               id={fieldUsernameId}
               name="username"
               type="text"
-              class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              class="input"
               placeholder="Username"
               bind:value={$form.username}
               {...$constraints.username}
             />
           </div>
-          <div>
-            <label
-              for={fieldPasswordId}
-              class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-            >
-              Password
-            </label>
+          <div class="space-y-2">
+            <label for={fieldPasswordId} class="label"> Password </label>
             <input
               type="password"
               name="password"
               id={fieldPasswordId}
               placeholder="••••••••"
-              class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              class="input"
               bind:value={$form.password}
               {...$constraints.password}
             />
           </div>
           <div class="flex items-center justify-between">
             <div class="flex items-start">
-              <div class="flex items-center h-5">
-                <input
-                  id="remember"
-                  aria-describedby="remember"
-                  type="checkbox"
-                  class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
-                  required=""
-                />
-              </div>
-              <div class="ml-3 text-sm">
-                <label for="remember" class="text-gray-500 dark:text-gray-300">Remember me</label>
-              </div>
+              <label class="flex items-center space-x-2">
+                <input class="checkbox checked:!bg-secondary-500" type="checkbox" />
+                <span class="text-sm">Remember me</span>
+              </label>
             </div>
             <a
-              href="#"
-              class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500"
-              >Forgot password?</a
+              href="/forgot-password"
+              class="text-sm font-medium text-secondary-600 hover:underline"
             >
+              Forgot password?
+            </a>
           </div>
           <button
             type="submit"
-            class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 flex items-center justify-center"
+            class="w-full btn variant-filled-primary"
             disabled={$signInMutation.status === "loading"}
           >
             <div>Sign in</div>
@@ -140,10 +112,8 @@
             {/if}
           </button>
           <p class="text-sm font-light text-gray-500 dark:text-gray-400">
-            Don’t have an account yet? <a
-              href="#"
-              class="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a
-            >
+            <span>Don’t have an account yet?&nbsp;</span>
+            <a href="/sign-up" class="font-medium text-secondary-600 hover:underline">Sign up</a>
           </p>
         </form>
       </div>
