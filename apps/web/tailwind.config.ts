2cff69943d61a35248b3dd78a328783da494286f
nurliman <nurlimandiara@gmail.com>
2023-09-02 19:38:20 +0700

    dynamically resolve folder for `@skeletonlabs/skeleton`

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -1,11 +1,12 @@
-import { Config } from "tailwindcss";
-import basePlugin from "./tailwind-base.cjs";
+import { join } from "node:path";
 import skeletonPlugin from "@skeletonlabs/skeleton/tailwind/skeleton.cjs";
+import basePlugin from "./tailwind-base.cjs";
+import type { Config } from "tailwindcss";
 
 export default {
   content: [
     "./src/**/*.{html,js,svelte,ts}",
-    "./node_modules/@skeletonlabs/skeleton/**/*.{html,js,svelte,ts}",
+    join(require.resolve("@skeletonlabs/skeleton"), "../**/*.{html,js,svelte,ts}"),
   ],
   darkMode: "class",
   plugins: [basePlugin(), ...skeletonPlugin()],
