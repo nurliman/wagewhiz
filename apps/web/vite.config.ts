893418dd0de19e9441eb9966adfa0d3c50adc356
nurliman <nurlimandiara@gmail.com>
2023-09-02 23:45:52 +0700

    install `purgecss`

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,10 +1,12 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
+import { purgeCss } from "vite-plugin-tailwind-purgecss";
 import Icons from "unplugin-icons/vite";
 
 export default defineConfig({
   plugins: [
     sveltekit(),
+    purgeCss(),
     Icons({
       compiler: "svelte",
     }),
