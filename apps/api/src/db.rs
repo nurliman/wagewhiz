9c18e4de21b33a964cb65336ad547171e43131fb
nurliman <nurlimandiara@gmail.com>
2024-02-12 01:12:25 +0700

    Refactor database module and update types

--- apps/api/src/db.rs
+++ apps/api/src/db.rs
@@ -1,13 +1,16 @@
 use crate::{env, errors::AppError};
+use bb8::{Pool, PooledConnection};
 use diesel_async::{pooled_connection::AsyncDieselConnectionManager, AsyncPgConnection};
 use std::time::Duration;
 use tokio::sync::OnceCell;
 
-pub type Pool = bb8::Pool<AsyncDieselConnectionManager<AsyncPgConnection>>;
+pub type DbPool = Pool<AsyncDieselConnectionManager<AsyncPgConnection>>;
 
-static DB: OnceCell<Pool> = OnceCell::const_new();
+pub type DbConn = PooledConnection<'static, AsyncDieselConnectionManager<AsyncPgConnection>>;
 
-async fn init_db() -> Result<Pool, AppError> {
+static DB: OnceCell<DbPool> = OnceCell::const_new();
+
+async fn init_db() -> Result<DbPool, AppError> {
     let env_var = env::get_env_var()?;
 
     let db_url = format!(
@@ -16,7 +19,7 @@ async fn init_db() -> Result<Pool, AppError> {
     );
 
     let config = AsyncDieselConnectionManager::<diesel_async::AsyncPgConnection>::new(db_url);
-    let pool = bb8::Pool::builder()
+    let pool = Pool::builder()
         .connection_timeout(Duration::from_secs(8))
         .build(config)
         .await
@@ -28,13 +31,11 @@ async fn init_db() -> Result<Pool, AppError> {
     Ok(pool)
 }
 
-pub async fn get_pool() -> Result<&'static Pool, AppError> {
+pub async fn get_pool() -> Result<&'static DbPool, AppError> {
     Ok(DB.get_or_init(|| async { init_db().await.unwrap() }).await)
 }
 
-pub async fn get_connection(
-) -> Result<bb8::PooledConnection<'static, AsyncDieselConnectionManager<AsyncPgConnection>>, AppError>
-{
+pub async fn get_connection() -> Result<DbConn, AppError> {
     let pool = get_pool().await?;
     let conn = pool.get().await.map_err(|e| {
         tracing::error!("Error getting connection: {:?}", e);
