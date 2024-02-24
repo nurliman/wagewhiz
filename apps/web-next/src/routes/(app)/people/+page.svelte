1f29603e255f1d57f9ab08ea75794f4b1d2adea8
nurliman <nurlimandiara@gmail.com>
2024-02-25 04:17:39 +0700

    create person card

--- apps/web-next/src/routes/(app)/people/+page.svelte
+++ apps/web-next/src/routes/(app)/people/+page.svelte
@@ -1,5 +1,44 @@
 <script lang="ts">
+  import PlusCircled from "svelte-radix/PlusCircled.svelte";
+  import MixerHorizontal from "svelte-radix/MixerHorizontal.svelte";
+  import { people } from "$lib/data/people";
+  import { Input } from "$lib/components/ui/input";
+  import { Button } from "$lib/components/ui/button";
+  import * as Card from "$lib/components/ui/card";
+  import Person from "./Person.svelte";
 </script>
 
-<h1>People</h1>
-<h2>Work in progress...</h2>
+<div>
+  <div class="container">
+    <Card.Root>
+      <Card.Header class="pb-8">
+        <Card.Title class="text-xl font-medium tracking-tight"
+          >&ldquo;Imagine all the <span class="text-3xl font-semibold">People</span>...</Card.Title
+        >
+        <Card.Description>
+          <span>...living life in peace.&rdquo;</span>
+          <span class="italic">&mdash; John Lennon (1971), &ldquo;Imagine&rdquo;</span>
+        </Card.Description>
+      </Card.Header>
+      <Card.Content>
+        <div class="mb-4 flex flex-row items-center space-x-2">
+          <Input type="search" placeholder="Search for people" class="h-8 w-[150px] lg:w-[250px]" />
+          <Button variant="outline" size="sm" class="h-8 border-dashed">
+            <PlusCircled class="mr-2 h-4 w-4" />
+            <span>Person</span>
+          </Button>
+          <div class="flex-1" />
+          <Button variant="outline" size="sm" class="h-8">
+            <MixerHorizontal class="mr-2 h-4 w-4" />
+            <span>View</span>
+          </Button>
+        </div>
+        <div class="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+          {#each people as person (person.id)}
+            <Person {person} />
+          {/each}
+        </div>
+      </Card.Content>
+    </Card.Root>
+  </div>
+</div>
---
+++ apps/web-next/src/routes/(app)/people/Person.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+  import type { Employee } from "$lib/types";
+  import { Button } from "$lib/components/ui/button";
+  import { Badge } from "$lib/components/ui/badge";
+  import DotsVertical from "svelte-radix/DotsVertical.svelte";
+  import PaperPlane from "svelte-radix/PaperPlane.svelte";
+  import * as Avatar from "$lib/components/ui/avatar";
+  import * as Card from "$lib/components/ui/card";
+  import initials from "initials";
+
+  export let person: Employee;
+
+  const personInitials = initials(person.name);
+  const personHref = `/people/${person.id}`;
+</script>
+
+<Card.Root>
+  <Card.Content>
+    <div class="mb-6" />
+    <div class="flex flex-col items-center space-y-4">
+      <a href={personHref}>
+        <Avatar.Root class="h-20 w-20">
+          <Avatar.Image src="https://github.com/nurliman.png" alt="@nurliman" loading="lazy" />
+          <Avatar.Fallback class="text-2xl">{personInitials}</Avatar.Fallback>
+        </Avatar.Root>
+      </a>
+      <div class="text-center">
+        <Badge variant="outline" class="mb-1.5">{person.status}</Badge>
+        <a href={personHref}>
+          <div class="text-lg font-semibold hover:underline">{person.name}</div>
+          <div class="text-muted-foreground text-sm">{person.position}</div>
+        </a>
+      </div>
+    </div>
+  </Card.Content>
+  <Card.Footer class="flex space-x-2">
+    <Button variant="secondary" size="sm" class="flex-1 h-8">
+      <PaperPlane class="mr-2 h-3.5 w-3.5" /> 
+      <span>Send Message</span>
+    </Button>
+    <Button variant="secondary" size="icon" class="h-8">
+      <DotsVertical class="h-4 w-4" />
+    </Button>
+  </Card.Footer>
+</Card.Root>
