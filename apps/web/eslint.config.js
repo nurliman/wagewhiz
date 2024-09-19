146d951df43f725b99f3af063206ea5810c1e41d
Nurliman Diara Aria <nurlimandiara@gmail.com>
2024-09-19 19:52:35 +0700

    chore: reorganize ignore files for Git, ESLint, Prettier, and Cursor

--- apps/web/eslint.config.js
+++ apps/web/eslint.config.js
@@ -1,1 +1,9 @@
-export { default } from "@wagewhiz/eslint-config";
+import eslintConfig from "@wagewhiz/eslint-config";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  ...eslintConfig,
+  {
+    ignores: ["src/lib/graphql/**/*", "!src/lib/graphql/execute.*"],
+  },
+];
